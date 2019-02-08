39290c3434102b5e7844ed5fd21d34a7d44a758f
HarshRaj0942 <f20170942@goa.bits-pilani.ac.in>
2019-02-08 21:41:03 +0530

    Bottom Nav Template made

---
+++ app/src/main/java/com/sabrcare/app/HomeActivity.java
@@ -0,0 +1,74 @@
+package com.sabrcare.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity  {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+
+                case R.id.navigation_timeline:
+                    fragment = new TimelineFragment();
+                    break;
+
+                case R.id.navigation_notifications:
+                    fragment = new NotificationFragment();
+                    break;
+
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+                    break;
+                case R.id.navigation_symptom_tracker:
+                    fragment = new SymptomTrackerFragment();
+            }
+
+            return loadFragment(fragment);
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        loadFragment(new HomeFragment());
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/sabrcare/app/SignUpActivity.java
+++ app/src/main/java/com/sabrcare/app/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.sabrcare.app;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,10 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(SignUpActivity.this,"REGISTERED!",Toast.LENGTH_LONG).show();
+
+                Intent launchHome = new Intent(SignUpActivity.this,HomeActivity.class);
+                startActivity(launchHome);
+                finish();
             }
         });
 
