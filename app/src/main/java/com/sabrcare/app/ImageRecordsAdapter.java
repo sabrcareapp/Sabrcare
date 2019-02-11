00085e54b9a6be317eaa744f76acd63dd47b19eb
[Hariram R] <[hari.2599@gmail.com]>
2019-02-11 18:15:01 +0530

    Records Fragment made.

---
+++ app/src/main/java/com/sabrcare/app/ImageRecordsAdapter.java
@@ -0,0 +1,6 @@
+package com.sabrcare.app;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ImageRecordsAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/sabrcare/app/RecordsAdapter.java
@@ -0,0 +1,6 @@
+package com.sabrcare.app;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecordsAdapter extends RecyclerView.Adapter {
+}
