db04cb433ef74a463483711368d5bfe3c659b8a8
HarshRaj0942 <f20170942@goa.bits-pilani.ac.in>
2019-02-15 03:52:24 +0530

    New Medication Fragment made, FAB added to Medicine Fragment

--- app/src/main/java/com/sabrcare/app/fragments/MedicineFragment.java
+++ app/src/main/java/com/sabrcare/app/fragments/MedicineFragment.java
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.clans.fab.FloatingActionMenu;
 import com.sabrcare.app.R;
 import com.sabrcare.app.adapters.MedicineAdapter;
 import com.sabrcare.app.models.MedicineModel;
@@ -19,10 +23,19 @@ import java.util.ArrayList;
 
 public class MedicineFragment extends Fragment {
 
+    com.github.clans.fab.FloatingActionButton fabNewMed;
+    FloatingActionMenu materialDesignFAM;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_medicine, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.medicine_rv);
+
+         materialDesignFAM = view.findViewById(R.id.material_design_android_floating_action_menu);
+         fabNewMed = view.findViewById(R.id.NewMedicineFAB);
+
+
+
 
 
 
@@ -38,6 +51,28 @@ public class MedicineFragment extends Fragment {
 
         MedicineAdapter medicineAdapter = new MedicineAdapter(medicine,getContext());
         recyclerView.setAdapter(medicineAdapter);
+
+        fabNewMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                AlertDialog.Builder newFolderDialogBuilder = new AlertDialog.Builder(getContext());
+                newFolderDialogBuilder.setView(R.layout.fragment_new_medication);
+                newFolderDialogBuilder.setCancelable(false);
+                newFolderDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setNegativeButton("Cancel",null);
+
+                AlertDialog newFolderDialog = newFolderDialogBuilder.create();
+                newFolderDialog.show();
+            }
+        });
+
         return view;
+
     }
     }
