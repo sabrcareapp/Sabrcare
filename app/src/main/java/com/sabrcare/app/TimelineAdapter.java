8eb2c79ee52025ac8d5298d5c0a917ee316598b5
HarshRaj0942 <f20170942@goa.bits-pilani.ac.in>
2019-02-14 20:07:27 +0530

    TimelineFragment Recyclerview Sample made

---
+++ app/src/main/java/com/sabrcare/app/TimelineAdapter.java
@@ -0,0 +1,59 @@
+package com.sabrcare.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.TimelineVH> {
+
+    private ArrayList<TimelineModel> timeline;
+    private Context ctx;
+    public TimelineAdapter(ArrayList<TimelineModel> timeline,Context ctx) {
+
+        this.ctx=ctx;
+        this.timeline=timeline;
+    }
+
+    @NonNull
+    @Override
+    public TimelineVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new TimelineAdapter.TimelineVH(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_timeline,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TimelineVH holder, int position) {
+        holder.title.setText(timeline.get(position).getTitle());
+        holder.subtitle.setText(timeline.get(position).getSubtitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return timeline.size();
+    }
+
+    public class TimelineVH extends RecyclerView.ViewHolder {
+
+        public TextView title;
+        public TextView subtitle;
+        public ImageView pic;               //will be changed to using Fresco library later
+
+        public TimelineVH(@NonNull View itemView) {
+            super(itemView);
+            title=itemView.findViewById(R.id.title);
+            subtitle=itemView.findViewById(R.id.subtitle);
+            pic=itemView.findViewById(R.id.pic);
+
+        }
+
+    }
+}
--- app/src/main/java/com/sabrcare/app/TimelineFragment.java
+++ app/src/main/java/com/sabrcare/app/TimelineFragment.java
@@ -2,21 +2,42 @@ package com.sabrcare.app;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 
 public class TimelineFragment extends Fragment {
 
+    private ArrayList<TimelineModel> timeline;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_timeline, null);
+
+        View view= inflater.inflate(R.layout.fragment_timeline, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.timeline_rv);
+
+
+
+        timeline = new ArrayList<>();
+
+        //generate 10 items just for now.WIll be changed later
+        for(int i=0;i<10;i++) {
+            timeline.add(new TimelineModel("title", "subtitle"));
+        }
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        TimelineAdapter timelineAdapter = new TimelineAdapter(timeline,getContext());
+        recyclerView.setAdapter(timelineAdapter);
+        return view;
     }
 }
