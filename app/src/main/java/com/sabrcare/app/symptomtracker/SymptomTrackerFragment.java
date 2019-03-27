121d02c843d650537da0937f55a863674a3dc5b9
2017A7PS0078G <f20170078@goa.bits-pilani.ac.in>
2019-03-27 22:05:48 +0530

    done

--- app/src/main/java/com/sabrcare/app/symptomtracker/SymptomTrackerFragment.java
+++ app/src/main/java/com/sabrcare/app/symptomtracker/SymptomTrackerFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -44,6 +45,8 @@ public class SymptomTrackerFragment extends Fragment {
 
     public int flag=0;
     private RequestQueue symptomQueue;
+    SharedPreferences sharedPreferences=getActivity().getPreferences(Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor=sharedPreferences.edit();
     private Map<String,String> symptomHeaders = new ArrayMap<String, String>();
 
     SharedPreferences setting;
@@ -143,7 +146,7 @@ public class SymptomTrackerFragment extends Fragment {
         symptomQueue = Volley.newRequestQueue(getContext());
         String url = getResources().getString(R.string.apiUrl)+"symptom/add";
         symptomHeaders.put("token",token);
-        //   symptomHeaders.put("token","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiaGFyaS4yNTk5QGdtYWlsLmNvLmluIiwiZXhwIjoxNTU0Mjk4OTUyfQ.qy7W-tdcSVGrEoZrNialM4VFURvX3UJ9o6Ifde5HN6s");
+        //   symptomHeaders.put("token","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiaGFyaS4yNTk5QGdtYWlsLmNvLmluIiwiZXhwIjoxNTU0Mjk4OTUyfQ.qy7W-tdcSVGrEoZrNialM4VFURvX3UJ9o6Ifde5HN6s")
         symptomHeaders.put("symptomArray",symptomArray.toString());
         StringRequest symptomAddition = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
