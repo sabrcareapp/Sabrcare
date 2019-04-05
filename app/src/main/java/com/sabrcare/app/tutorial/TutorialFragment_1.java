531094c6b683b57b30d24e5d63cfb44dc9d8c6ae
[Hariram R] <[hari.2599@gmail.com]>
2019-04-05 23:46:00 +0530

    v1 issues resolve

--- app/src/main/java/com/sabrcare/app/tutorial/TutorialFragment_1.java
+++ app/src/main/java/com/sabrcare/app/tutorial/TutorialFragment_1.java
@@ -1,9 +1,11 @@
 package com.sabrcare.app.tutorial;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.sabrcare.app.R;
 
@@ -13,11 +15,15 @@ import androidx.fragment.app.Fragment;
 
 public class TutorialFragment_1 extends Fragment {
 
+    ImageView icon;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tutorial_1, container, false);
+        View view= inflater.inflate(R.layout.fragment_tutorial_1, container, false);
+        icon = view.findViewById(R.id.img1);
+        icon.setColorFilter(Color.parseColor("#2C8BBF"));
+        return view;
     }
 }
--- app/src/main/java/com/sabrcare/app/tutorial/TutorialFragment_2.java
+++ app/src/main/java/com/sabrcare/app/tutorial/TutorialFragment_2.java
@@ -1,10 +1,12 @@
 package com.sabrcare.app.tutorial;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.sabrcare.app.R;
 
@@ -15,11 +17,15 @@ import androidx.fragment.app.Fragment;
 public class TutorialFragment_2 extends Fragment {
 
 
+    ImageView icon;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tutorial_2, container, false);
+        View view= inflater.inflate(R.layout.fragment_tutorial_2, container, false);
+        icon = view.findViewById(R.id.img2);
+        icon.setColorFilter(Color.parseColor("#2C8BBF"));
+        return view;
     }
 
 }
